e4e6081fe03feeaf37f319a4c764a8d2a3a9ce7d
nguyen <daybantoima@gmail.com>
2016-08-10 18:12:12 +0700

    update, add new AddNewGroupActivity

--- app/src/main/java/offic/htk/com/officproject/activitis/AddNewGroupActivity.java
+++ app/src/main/java/offic/htk/com/officproject/activitis/AddNewGroupActivity.java
@@ -2,14 +2,32 @@ package offic.htk.com.officproject.activitis;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import offic.htk.com.officproject.R;
 
-public class AddNewGroupActivity extends AppCompatActivity {
+public class AddNewGroupActivity extends AppCompatActivity implements View.OnClickListener {
+    private TextView mTvCancel, mTvDone;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_new_group);
+        mTvCancel = (TextView) findViewById(R.id.tv_cancel_add_new_group);
+        mTvDone = (TextView) findViewById(R.id.tv_done_add_new_group);
+        mTvCancel.setOnClickListener(this);
+        mTvDone.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.tv_cancel_add_new_group:
+                AddNewGroupActivity.this.finish();
+                break;
+            case R.id.tv_done_add_new_group:
+                break;
+        }
     }
 }
--- app/src/main/java/offic/htk/com/officproject/fragments/FragmentGroups.java
+++ app/src/main/java/offic/htk/com/officproject/fragments/FragmentGroups.java
@@ -1,12 +1,15 @@
 package offic.htk.com.officproject.fragments;
 
+import android.content.Intent;
 import android.support.v4.app.FragmentTabHost;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import offic.htk.com.officproject.R;
+import offic.htk.com.officproject.activitis.AddNewGroupActivity;
 import offic.htk.com.officproject.activitis.MainActivity;
 import offic.htk.com.officproject.base.BaseFragment;
 
@@ -18,6 +21,7 @@ public class FragmentGroups extends BaseFragment implements View.OnClickListener
     private Toolbar toolbar;
     private TextView mTvEditProfile;
     private ImageView mImgAddNewContacts;
+    private Button mBtnAddNewGroup;
 
     @Override
     protected void onCreate() {
@@ -32,8 +36,10 @@ public class FragmentGroups extends BaseFragment implements View.OnClickListener
         mImgAddNewContacts = (ImageView) toolbar.findViewById(R.id.img_action_add);
         mImgAddNewContacts.setVisibility(View.VISIBLE);
         mTvEditProfile = (TextView) toolbar.findViewById(R.id.tv_action_edit);
+        mBtnAddNewGroup = (Button) view.findViewById(R.id.btn_create_your_first_groups);
         mTvEditProfile.setVisibility(View.GONE);
         mImgAddNewContacts.setOnClickListener(this);
+        mBtnAddNewGroup.setOnClickListener(this);
 
     }
 
@@ -46,6 +52,10 @@ public class FragmentGroups extends BaseFragment implements View.OnClickListener
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.img_action_add:
+                startActivity(new Intent(getContext(), AddNewGroupActivity.class));
+                break;
+            case R.id.btn_create_your_first_groups:
+                startActivity(new Intent(getContext(), AddNewGroupActivity.class));
                 break;
         }
     }
