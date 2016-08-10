1de1ef272de1d99615914810f454ec3606a75f6d
nguyen <daybantoima@gmail.com>
2016-08-10 17:34:24 +0700

    add new AddNewGroupActivity.java

---
+++ app/src/main/java/offic/htk/com/officproject/activitis/AddNewGroupActivity.java
@@ -0,0 +1,15 @@
+package offic.htk.com.officproject.activitis;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import offic.htk.com.officproject.R;
+
+public class AddNewGroupActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_new_group);
+    }
+}
